6f6a1fe426261097a1dbab39916f598c4264595b
SS56Bo <sohamsarkar345@gmail.com>
2026-04-28 23:56:56 +0530

    Template finished - Need to add camera controls

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import * as THREE from 'three';
 const renderer = new THREE.WebGLRenderer({antialias: true});
 
 // Initial viewport size
-const w = window.innerWidth;
-const h = window.innerHeight;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 // Set renderer size
-renderer.setSize(w, h);
+renderer.setSize(width, height);
 
 // Improve rendering on high-DPI displays (Retina, etc.)
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -23,21 +23,38 @@ document.body.appendChild(renderer.domElement);
 // Camera settings
 const camera = new THREE.PerspectiveCamera(
     75,     // Field of view (degrees)
-    w/h,    // Aspect Ratio (defines how wide the view is relative to its height)
+    width/height,    // Aspect Ratio (defines how wide the view is relative to its height)
     0.01,   // Near clipping plane
-    100     // Far clipping plane
+    10     // Far clipping plane
 );
 camera.position.z = 2; 
 
 //============= SCENE SETUP ==================
 const scene = new THREE.Scene();
 
+//============= MATERIALS ===================
+const geometry = new THREE.BoxGeometry(1,1,1)
+const mesh = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+});
+
+const meshObject = new THREE.Mesh(geometry, mesh);
+meshObject.scale.setScalar(1.001);
+
+//============= LIGHT ========================
+const hemilight = new THREE.HemisphereLight(0xff3211, 0x111111);
+
+//============= ADD TO SCENE ========================
+scene.add(meshObject);
+scene.add(hemilight);
 
 //============= ANIMATION LOOP ========================
 function animateScene() {
     requestAnimationFrame(animateScene);
-
+    // YOUR CODE HERE //
+    meshObject.rotation.y += 0.01; // added a little animation for development purposes
+    meshObject.rotation.x += 0.01;
     renderer.render(scene, camera);
 }
 
-animateScene()
+animateScene();
